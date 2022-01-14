1a32c1ec705347d59b24dd73fcee48017d537da0
Noelle Hsu <ohyanana2@gmail.com>
2022-01-14 22:57:08 +0800

    把錢給贏家

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,9 +5,12 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery is VRFConsumerBase, Ownable{
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     uint256 public usdEntryFee;
+    
     AggregatorV3Interface internal ethUsdPriceFeed;
     //enum回傳值0,1,2
     enum LOTTERY_STATE {
@@ -18,7 +21,7 @@ contract Lottery is VRFConsumerBase, Ownable{
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
     bytes32 public keyhash;
-    
+
     //constructor 裡面可以再寫一個繼承而來的constructor
     constructor(
         address _priceFeedAddress,
@@ -33,11 +36,13 @@ contract Lottery is VRFConsumerBase, Ownable{
         fee = _fee;
         keyhash = _keyhash;
     }
-    function enter() public payable{
+
+    function enter() public payable {
         require(lottery_state == LOTTERY_STATE.OPEN);
         require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
+
     // 美金換算ETH
     function getEntranceFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
@@ -48,14 +53,16 @@ contract Lottery is VRFConsumerBase, Ownable{
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
-    function startLottery() public onlyOwner{
+
+    function startLottery() public onlyOwner {
         require(
             lottery_state == LOTTERY_STATE.CLOSED,
             "Can't start a new lottery yet!"
         );
         lottery_state = LOTTERY_STATE.OPEN;
     }
-    function endLottery () public onlyOwner{
+
+    function endLottery() public onlyOwner {
         //這方法其實不夠隨機（偽隨機性）
         // uint256(
         //     keccack256(
@@ -72,12 +79,23 @@ contract Lottery is VRFConsumerBase, Ownable{
         // 因為有引用VRFConsumerBase，可以直接呼叫
         bytes32 requestId = requestRandomness(keyhash, fee);
     }
+
     //internal：只能由該合約內與繼承的合約呼叫（此方法只能讓chainlink Node呼叫）
-    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override{
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
         require(
             lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
             "You aren't there yet!"
         );
         require(_randomness > 0, "random-not-found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
     }
 }
